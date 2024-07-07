c55390f67ed44f78809ec2708e2439575902ed84
Jonathan Saravia Zurita <jona.saravia@duocuc.cl>
2024-07-07 00:05:11 -0400

    Update app.js

--- juego_numero_secreto_version_final/app.js
+++ juego_numero_secreto_version_final/app.js
@@ -1,35 +1,3 @@
-//con document.querySelector('nombre del selector en HTML') se hace la conexión con el objeto de HTML 
-// Se asigna a una variable (titulo) 
-//let titulo = document.querySelector('h1');
-// a la variable se le puede asignar un valor, como un string, con "inner.HTML"
-//titulo.innerHTML = 'Juego del número secreto ';
-
-//let parrafo = document.querySelector('p')
-//parrafo.innerHTML='Indica un número del 1 al 10'
-
-// Se puede usar arrays o listas en Javascript
-// Estas pueden tener distintos datos. Se sugiere que una lista contenga solo un tipo de dato y otra lista otro tipo de dato
-// El array se declara con let (nombre del array) = [] 
-//ejemplo let numerosSorteados=[];
-
-//Para agregar un elemento al final del array se usa el método push
-//numerosSorteados.push(5);
-//numerosSorteados=[5];
-
-//Para saber el tamaño o cantidad de elementos en un array, se ocupa
-//console.log(numerosSorteados.length);
-//El resultado será 1, ya que solo se ha ingresado un elemento.
-
-//Se puede acceder a un elemento con la ayuda del índice
-//Se puede hacer de esta forma console.log(numerosSorteados[0])
-//La posición 0 siempre es el primer elemento.
-
-//Podemos pedir el último elemento de la lista de esta forma:
-//console.log(numerosSorteados[numerosSorteados.length]-1);
-
-//Se puede eliminar el últimmo elemento de un array con pop
-//De esta manera numerosSorteados.pop();
-
 let numeroSecreto= 0;
 let intentos=0;
 let listaNumerosSorteados=[];
@@ -46,10 +14,6 @@ function asignarTextoElemento(elemento,texto){
 function verificarIntento(){
     //al encerrar en el parámetro en parseInt se transforma el string en un número entero.
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    //console.log(typeof(numeroSecreto))
-    //console.log(numeroSecreto);
-    //console.log(typeof(numeroDeUsuario))
-    //console.log(numeroDeUsuario);
     // Con el === se le pide a la máquina que la comparación tiene que ser igual en valor y en tipo de datos.
     ///console.log(numeroDeUsuario === numeroSecreto);
     //Con esto puedo utilizar una sola función, previamente definida, para encontrar el número secreto.
@@ -59,7 +23,6 @@ function verificarIntento(){
         //el atributo que tenga un elemento de HTML en Javascript
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else{
-        //console.log(intentos)
         //el usuario no acertó
         if (numeroDeUsuario>numeroSecreto){
             asignarTextoElemento('p','El número secreto es menor');
@@ -74,14 +37,9 @@ function verificarIntento(){
 
 //Con la función limpiar caja se elimina automáticamente el número no acertado del usuario
 function limpiarCaja(){
-    //Con que document.querySelector('#valorUsuario') también se puede llamar al ID
-    //let valorCaja= document.querySelector('#valorUsuario');
-    //valorCaja.value='';
-    //Esta es una forma más abreviada del código anterior.
     document.querySelector('#valorUsuario').value='';
 }
 
-//al escribir solo function, VS Code entrega la opción de function statement que genera la anatomía básica de toda función
 function generarNumeroSecreto() {
     let numeroGenerado= Math.floor(Math.random()*numeroMaximo)+1;
     console.log(numeroGenerado);
